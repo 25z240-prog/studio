a34cad39daa7d661bddbfac2c323d77c95106379
NISHANTH R <25z240@psgitech.ac.in>
2025-11-25 03:37:32 +0000

    replace the mentioned image with idly https://chakriskitchen.com/wp-cont

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         hostname: "encrypted-tbn0.gstatic.com",
         port: "",
         pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "chakriskitchen.com",
+        port: "",
+        pathname: "/**",
       }
     ],
   },
