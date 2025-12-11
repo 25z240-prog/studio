ca76cfbee2fff51ec0f98451d69a501313951746
NISHANTH R <25z240@psgitech.ac.in>
2025-12-11 16:04:59 +0000

    Revert the changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,6 @@ export const firebaseConfig = {
   "appId": "1:643656207718:web:7893cc5c2983b802930ca7",
   "apiKey": "AIzaSyDnxuI1W3QFofg1MYV5GH6I-2KgrB8Jhuc",
   "authDomain": "studio-6119922862-3bb67.firebaseapp.com",
-  "storageBucket": "studio-6119922862-3bb67.appspot.com",
   "measurementId": "",
   "messagingSenderId": "643656207718"
 };
